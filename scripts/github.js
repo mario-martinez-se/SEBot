c5c07d801818dfb2427ac3abddd433f6c42c2dbb
Mario M <mario.martinez@secretescapes.com>
2018-04-06 16:10:30 +0200

    test

--- scripts/github.js
+++ scripts/github.js
@@ -1,6 +1,6 @@
 module.exports = (robot) => {
 
-  robot.hear(/https:\/\/github.com\/[^\/]*\/[^\/]*\/pull/(\d+)\/.*/g, [], (msg)=> {
+  robot.hear(/https:\/\/github.com\/[^\/]*\/[^\/]*\/pull\/(\d+)\/.*/g, [], (msg)=> {
     msg.reply("world");
   });
 
