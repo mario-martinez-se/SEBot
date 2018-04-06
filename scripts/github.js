cd0355515d1bcf246c8d67dd625ec693c44fc32b
Mario M <mario.martinez@secretescapes.com>
2018-04-06 17:25:28 +0200

    test

--- scripts/github.js
+++ scripts/github.js
@@ -10,6 +10,38 @@ module.exports = (robot) => {
     const owner = match[1];
     const  repo = match[2];
     const number = match[3];
+
+    const test = {
+      "attachments": [
+        {
+          "fallback": "adding booking with flights to await payment job",
+          "color": "#36a64f",
+          "author_name": "Nelson Gallardo",
+          "author_link": "https://github.com/nelson687se",
+          "author_icon": "https://avatars2.githubusercontent.com/u/15727269?v=4",
+          "title": "adding booking with flights to await payment job",
+          "title_link": "https://github.com/secretescapes/secret-escapes/pull/2004",
+          "text": "Optional text that appears within the attachment",
+          "fields": [
+            {
+              "title": "Status",
+              "value": "Open",
+              "short": true
+            },
+            {
+              "title": "Lines changed",
+              "value": "+1234 -3123"
+              "short": true
+            }
+          ],
+          "image_url": "http://my-website.com/path/to/image.jpg",
+          "thumb_url": "https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png",
+          "ts": 123456789
+        }
+      ]
+    };
+
+
     if (owner && repo && number) {
       rp({
         method: "GET",
@@ -18,35 +50,7 @@ module.exports = (robot) => {
           "Authorization": `token ${GITHUB_TOKEN}`,
           "User-Agent": "SEBOT"
         }
-      }).then(data=> res.reply({
-        "attachments": [
-          {
-            "fallback": "adding booking with flights to await payment job",
-            "color": "#36a64f",
-            "author_name": "Nelson Gallardo",
-            "author_link": "https://github.com/nelson687se",
-            "author_icon": "https://avatars2.githubusercontent.com/u/15727269?v=4",
-            "title": "adding booking with flights to await payment job",
-            "title_link": "https://github.com/secretescapes/secret-escapes/pull/2004",
-            "text": "Optional text that appears within the attachment",
-            "fields": [
-              {
-                "title": "Status",
-                "value": "Open",
-                "short": true
-              },
-              {
-                "title": "Lines changed",
-                "value": "+1234 -3123",
-                "short": true
-              }
-            ],
-            "image_url": "http://my-website.com/path/to/image.jpg",
-            "thumb_url": "https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png",
-            "ts": 123456789
-          }
-        ]
-      }))
+      }).then(data=> res.emit('slack-attachment', test))
     }
   });
 
