a50f05e6a059987187cc4588e8c1d814b4ae3ae0
Mario M <mario.martinez@secretescapes.com>
2018-04-06 17:43:22 +0200

    test

--- scripts/github.js
+++ scripts/github.js
@@ -52,8 +52,8 @@ module.exports = (robot) => {
         }
       ]
     };
-
-    robot.adapter.client.web.chat.postMessage(msg.message.room, "This is a message!", {as_user: true, unfurl_links: false})
+    msg.reply("HELLO")
+    // robot.adapter.client.web.chat.postMessage(msg.message.room, "This is a message!", {as_user: true, unfurl_links: false})
 
 
     // if (owner && repo && number) {
