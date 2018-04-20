4a2da21777b87d90726598d367c9d7d12d274991
Mario <mario.martinez@secretescapes.com>
2018-04-20 12:28:01 +0200

    test

--- scripts/github.js
+++ scripts/github.js
@@ -10,14 +10,14 @@ module.exports = (robot) => {
 
 
     console.log(res.match
-      .map(match => ({owner: match[1], repo: match[2], number: match[3]}))
+      .map(data => ({owner: data[1], repo: data[2], number: data[3]}))
       .filter(data => data.owner && data.repo && data.number));
-
-    Promise.all(res.match
-      .map(match => ({owner: match[1], repo: match[2], number: match[3]}))
-      .filter(data => data.owner && data.repo && data.number)
-      .map(data => rp(githubRequest({owner: data.owner, repo: data.repo, number: data.number})))).then(data => {
-    });
+	//
+    // Promise.all(res.match
+    //   .map(match => ({owner: match[1], repo: match[2], number: match[3]}))
+    //   .filter(data => data.owner && data.repo && data.number)
+    //   .map(data => rp(githubRequest({owner: data.owner, repo: data.repo, number: data.number})))).then(data => {
+    // });
 
     const match = regex.exec(res.match[0]);
     const owner = match[1];
