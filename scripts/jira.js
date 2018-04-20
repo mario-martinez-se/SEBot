bff35585795d8e2f1072d5252cda6ffea0774dd2
Mario <mario.martinez@secretescapes.com>
2018-04-20 16:04:41 +0200

    test

--- scripts/jira.js
+++ scripts/jira.js
@@ -12,7 +12,7 @@ module.exports = (robot) => {
     Promise.all(
       res.match.map(issueId => rp(jiraRequest(issueId)))
     )
-    .then(values => res.send(`${values}`));
+    .then(values => robot.adapter.client.web.chat.postMessage(res.message.room, message(values), {as_user: true, unfurl_links: false, attachments: attachments(values)}));
 
   });
 };
@@ -20,10 +20,40 @@ module.exports = (robot) => {
 
 const jiraRequest = (issueId) => ({
   method: "GET",
-  uri: `https://secretescapes.atlassian.net/rest/api/2/issue/${issueId}?fields=summary`,
+  uri: `https://secretescapes.atlassian.net/rest/api/2/issue/${issueId}?fields=summary,assignee`,
   headers: {
     "Authorization": `Basic ${encode.encode(JIRA_USERNAME+":"+JIRA_TOKEN, 'base64')}`,
     "User-Agent": "SEBOT",
     "Content-Type": "application/json"
   }
+});
+
+
+const message = (responses) => `Hey! Are you talking about ${responses.length < 2 ? 'this' : 'these'} JIRA ticket${responses.length < 2 ? '' : 's'}: ${responses.map(response => JSON.parse(response).key).join(', ')}?`;
+
+const attachments = (responses) => responses.map(response => attachment(JSON.parse(response)));
+
+const attachment = (response) => ({
+  "fallback": `${data.fields.summary}`,
+  "title": `${data.fields.summary}`,
+  "color": "#2cbe4e",
+  "author_name": `${data.fields.assignee.displayName}`,
+  // "author_link": `${data.user.html_url}`,
+  "author_icon": `${data.fields.assignee.avatarUrls.16x16}`,
+  // "title_link": `${data.html_url}`,
+  "text": `${data.fields.description}`,
+  // "fields": [
+  //   {
+  //     "title": "Status",
+  //     "value": `${data.state}`,
+  //     "short": true
+  //   },
+  //   {
+  //     "title": "Lines changed",
+  //     "value": `+${data.additions} -${data.deletions}`,
+  //     "short": true
+  //   }
+  // ],
+  "thumb_url": "https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png",
+  "ts": Date.parse(data.created_at)/1000
 });
