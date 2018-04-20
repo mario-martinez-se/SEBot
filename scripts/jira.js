883d6e3d50cc66eb137fb003d80e6cd2fea43bec
Mario <mario.martinez@secretescapes.com>
2018-04-20 13:32:20 +0200

    test

--- scripts/jira.js
+++ scripts/jira.js
@@ -2,8 +2,8 @@ const rp = require('request-promise');
 require('dotenv').config();
 
 module.exports = (robot) => {
-  const regexGeneral = /DEV-(\d+)/g;
-  const regex = /DEV-(\d+)/;
+  const regexGeneral = /DEV-\d+/g;
+  const regex = /DEV-\d+/;
   robot.hear(regexGeneral, [], (res)=> {
 
     res.send(`${res.match.map(issueId => regex.exec(issueId))}`);
