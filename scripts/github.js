6cd473d20432946d6720417777617da9b5d8ddf4
Mario <mario.martinez@secretescapes.com>
2018-04-20 11:25:52 +0200

    test

--- scripts/github.js
+++ scripts/github.js
@@ -12,46 +12,35 @@ module.exports = (robot) => {
 
   const regex = /https:\/\/github.com\/([^\/]*)\/([^\/]*)\/pull\/(\d+)\/?/g;
   robot.hear(regex, [], (res)=> {
-    robot.adapter.client.web.chat.postMessage(res.message.room, "This is a message!", {as_user: true, unfurl_links: false, attachments: [
-        {
-          title: 'a title',
-          fallback: 'a fallback',
-          title_link: 'http://example.com'
-        }
-      ]});
-   /* const match = regex.exec(res.match[0]);
+    const match = regex.exec(res.match[0]);
     const owner = match[1];
     const  repo = match[2];
     const number = match[3];
 
     const test = {
-      "attachments": [
+      "fallback": "adding booking with flights to await payment job",
+      "color": "#36a64f",
+      "author_name": "Nelson Gallardo",
+      "author_link": "https://github.com/nelson687se",
+      "author_icon": "https://avatars2.githubusercontent.com/u/15727269?v=4",
+      "title": "adding booking with flights to await payment job",
+      "title_link": "https://github.com/secretescapes/secret-escapes/pull/2004",
+      "text": "Optional text that appears within the attachment",
+      "fields": [
         {
-          "fallback": "adding booking with flights to await payment job",
-          "color": "#36a64f",
-          "author_name": "Nelson Gallardo",
-          "author_link": "https://github.com/nelson687se",
-          "author_icon": "https://avatars2.githubusercontent.com/u/15727269?v=4",
-          "title": "adding booking with flights to await payment job",
-          "title_link": "https://github.com/secretescapes/secret-escapes/pull/2004",
-          "text": "Optional text that appears within the attachment",
-          "fields": [
-            {
-              "title": "Status",
-              "value": "Open",
-              "short": true
-            },
-            {
-              "title": "Lines changed",
-              "value": "+1234 -3123",
-              "short": true
-            }
-          ],
-          "image_url": "http://my-website.com/path/to/image.jpg",
-          "thumb_url": "https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png",
-          "ts": 123456789
+          "title": "Status",
+          "value": "Open",
+          "short": true
+        },
+        {
+          "title": "Lines changed",
+          "value": "+1234 -3123",
+          "short": true
         }
-      ]
+      ],
+      "image_url": "http://my-website.com/path/to/image.jpg",
+      "thumb_url": "https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png",
+      "ts": 123456789
     };
 
     msgData = {
@@ -65,10 +54,9 @@ module.exports = (robot) => {
         }
       ]
     };
+    robot.adapter.client.web.chat.postMessage(res.message.room, "This is a message!", {as_user: true, unfurl_links: false, attachments: [test]});
+    // robot.adapter.client.web.chat.postMessage(res.message.room, "This is a message!", {as_user: true, unfurl_links: false})
 
-    robot.adapter.client.web.chat.postMessage(res.message.room, "This is a message!", {as_user: true, unfurl_links: false})
-
-*/
     // if (owner && repo && number) {
     //   rp({
     //     method: "GET",
