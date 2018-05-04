bc56cebd4f075440cf643011a53d6d400398cd6e
Mario <mario.martinez@secretescapes.com>
2018-05-04 13:46:47 +0200

    changes

--- scripts/self_config.js
+++ scripts/self_config.js
@@ -3,16 +3,13 @@ var juration = require('juration');
 module.exports = (robot) => {
   const regex = /be quiet for (.*)/i;
   robot.respond(regex, [], (res)=> {
+    const secs = juration.parse(res.match[1]);
 
-    res.send(`${juration.parse(res.match[1])}`)
-
-    // const secs = res.match[1];
-	//
-    // if (!isNaN(secs)) {
-    //   robot.brain.set(`SILENT_FOR:${res.message.room}`, secs);
-    //   res.send(`Ok! I won't repeat myself for at least ${secs} secs in this channel`);
-    // } else {
-    //   res.send(`Sorry, I don't understand`);
-    // }
+    if (!isNaN(secs)) {
+      robot.brain.set(`SILENT_FOR:${res.message.room}`, secs);
+      res.send(`Ok! I won't repeat myself for at least ${secs} secs in this channel`);
+    } else {
+      res.send(`Sorry, I don't understand`);
+    }
   });
 };
