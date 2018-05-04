3efcbe9932c27b61c7bad590e0284d380feabbc3
Mario <mario.martinez@secretescapes.com>
2018-05-04 13:17:52 +0200

    changes

--- scripts/self_config.js
+++ scripts/self_config.js
@@ -3,7 +3,7 @@ module.exports = (robot) => {
   const regex = /be quiet for (.*)/i;
   robot.respond(regex, [], (res)=> {
 
-    const secs = match[1];
+    const secs = res.match[1];
     robot.brain.set(`SILENT_FOR:${res.message.room}`, secs);
     res.send(`Ok! I won't repeat myself for at least ${secs} secs in this channel`);
   });
