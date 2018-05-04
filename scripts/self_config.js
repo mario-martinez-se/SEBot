92a638f230f678768204fa35be5cd4719e209413
Mario <mario.martinez@secretescapes.com>
2018-05-04 15:12:21 +0200

    changes

--- scripts/self_config.js
+++ scripts/self_config.js
@@ -12,8 +12,7 @@ module.exports = (robot) => {
     }
   });
 
-  robot.respond(/config/i, [], (res) => {
-    console.log(robot.brain.set(`SILENT_FOR:${res.message.room}`));
-    res.send(`I won't repeat myself for ${robot.brain.set(`SILENT_FOR:${res.message.room}`)||commons.DEFAULT_MUTE_PERIOD} secs in this channel`)
+  robot.respond(/config/i, [], (res) =>
+    res.send(`I won't repeat myself for ${robot.brain.get(`SILENT_FOR:${res.message.room}`)||commons.DEFAULT_MUTE_PERIOD} secs in this channel`)
   });
 };
