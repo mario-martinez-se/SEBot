8b2c13a5cfdae9b45a3850fb80730981dedc992f
Mario <mario.martinez@secretescapes.com>
2018-05-04 16:45:50 +0200

    changes

--- commons.js
+++ commons.js
@@ -3,15 +3,15 @@ const client = require('redis').createClient(process.env.REDISCLOUD_URL);
 const mgetAsync = promisify(client.mget).bind(client);
 const _ = require("underscore");
 
+const DEFAULT_MUTE_PERIOD = 30;
 module.exports = {
   filterByExpirity: (allKeys, channel, brain) =>
   mgetAsync(allKeys.map(key => `${key}:${channel}`))
   .then(values => _.zip(allKeys, values))
   .then(pairs => pairs.filter(pair => pair[1] == null).map(pair => pair[0]))
   .then(keys => {
-    keys.map(key => client.set(`${key}:${channel}`, "OK", "EX", brain.get(`SILENT_FOR:${channel}`)||this.DEFAULT_MUTE_PERIOD));
+    keys.map(key => client.set(`${key}:${channel}`, "OK", "EX", brain.get(`SILENT_FOR:${channel}`)||DEFAULT_MUTE_PERIOD));
     return keys;
   }),
-  getMutePeriod: (brain, channel) => brain.get(`SILENT_FOR:${channel}`)||this.DEFAULT_MUTE_PERIOD,
-  DEFAULT_MUTE_PERIOD: 30
+  getMutePeriod: (brain, channel) => brain.get(`SILENT_FOR:${channel}`)||DEFAULT_MUTE_PERIOD
 };
