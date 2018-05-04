5733c05f8d87f786d339d8851957ed65db08d100
Mario <mario.martinez@secretescapes.com>
2018-05-04 13:01:29 +0200

    changes

--- scripts/self_config.js
+++ scripts/self_config.js
@@ -1,7 +1,8 @@
+var parser = require('moment-parser');
 
 module.exports = (robot) => {
-  const regex = /badger/i;
+  const regex = /be quiet for (.*)/gi;
   robot.respond(regex, [], (res)=> {
-    res.send(`Ok`)
+    res.send(`Ok: ${res.match}`)
   });
 };
